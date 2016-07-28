58e3531a3231d431f2d6a860b7c8257f8f136209
fsmsystems <fsm.systems@gmail.com>
2016-07-28 11:39:33 +0200

    new changes

--- certcheck.py
+++ certcheck.py
@@ -23,68 +23,72 @@ import os
 import datetime
 import sys
 import getopt
+import subprocess
+from subprocess import PIPE,Popen
 
-def check_date_with_wcparams(obj_expiredate,date_today,diff_date,value_warning,value_critical):
-    if diff_date.days > value_warning:
-        return 0 # OK
-    else:
-        if diff_date.days <= value_warning:
-            if diff_date.days >= value_critical:
-                return 2 # CRITICAL
-            else:
-                return 1 # WARNING
 
-def print_to_stdout(status,diff_date,silent):
+def print_to_stdout(diff_date,silent,obj_expiredate):
     if silent: # if only want to see value without logics 
-        print str(diff_date.days)
-    else: # Normal view
-        out_str=str(diff_date.days) + ' days left for '+_host
-        if status == 0:
-            print 'OK: '+out_str
-        if status == 1:
-            print 'WARNING: '+out_str
-        if status == 2:
-            print 'CRITICAL: '+out_str
+        print str(diff_date.days)+','+str(obj_expiredate).split(' ')[0]
+    else:
+        print str(diff_date.days) +' days'
 
 def usage(version):
-        print 'Certcheck '+version+ '(FSM)'
+        print 'Certcheck '+version+ '(LLSVDC)'
         print 'Usage: certcheck -H [Hostname or IP] -p [port] -w [days warning] -c [days critical] -s (silent)' 
         print ' -H --host: Hostname or IP'
         print ' -p --port: HTTPS port'
-        print ' -w or -c: Warning and Critical days to expire optional values'
-        print ' -s --silent: Returns only the difference in days between Now and expire date'
+        print ' -s --silent: Returns only the number of the days remaining'
+        print 'SEE THE MAN PAGE (https://github.com/fsmsystems/certcheck)  FOR MORE OPTIONS AND EXAMPLES'
         sys.exit(0)
 
-if __name__ == "__main__":
-        value_warning=int(30)
-        value_critical=int(15)
-        version = '0.0.2'
-        silent = False
-        options, remainder = getopt.gnu_getopt(sys.argv[1:], 'H:p:w:c:sh', ['Host=','port=','warning=','critical=',])
-        #print 'OPTIONS   :', options
 
-        for opt, arg in options:
+def search_openssl():
+    openssl_bin = os.popen('whereis openssl').read().split()[1]
+    return openssl_bin
+
+if __name__ == "__main__":
+        version = '0.0.4'       # Version :)
+        openssl_bin = search_openssl() # serach openssl binary with system whereis
+        silent = False          # 
+        _port = '443'           # Default port if is not passed as parameter
+
+        try: 
+            options, remainder = getopt.gnu_getopt(sys.argv[1:], 'H:p:hs', ['Host=','port=',])
+        except getopt.GetoptError,e:
+            print 'LOL: You miss something!!',e
+            sys.exit(1)
+        #print 'OPTIONS   :', options
+        if options:     # don't die with blank parameters
+         for opt, arg in options:
             if opt in ('-h', '--help'):
                 usage(version)
             elif opt in ('-H','--host'):
-                _host = arg
+                 _host = arg
             elif opt in ('-p', '--port'):
                 _port = arg
             elif opt in ('-s', '--silent'):
                 silent = True
-            elif opt in ('-w', '--warn'):
-                value_warning = int(arg)
-            elif opt in ('-c', '--crit'):
-                value_critical = int(arg)
+        else:
+             usage(version) # like good commands
 
+        date_today = datetime.datetime.now()    # Today Now date to compare
 
-        date_today = datetime.datetime.now()#.strftime('%b %d %H:%M:%S %Y %Z')
-        openssl_cmd='echo | openssl s_client -connect '+_host+':'+_port+' 2>/dev/null | openssl x509 -noout -dates'
-        proc = os.popen(openssl_cmd).read().split('\n')
-        
-        expiredate = proc[1].split('=')[1]   # expiredate from cert
-        obj_expiredate = datetime.datetime.strptime(expiredate, '%b %d %H:%M:%S %Y %Z') # object transformation
-        diff_date = obj_expiredate - date_today # difference of obj_expiredate - date_today
-        
-        status = check_date_with_wcparams(obj_expiredate,date_today,diff_date,value_warning,value_critical) # Devuelve si es OK,W,C
-        print_to_stdout(status,diff_date,silent) # Salida
+        try:    # This will be the command to send to system with openssl
+            openssl_cmd='echo | '+openssl_bin+' s_client -connect '+str(_host)+':'+str(_port)+' 2>/dev/null | '+openssl_bin+' x509 -noout -dates'
+        except NameError:
+            print "Check the inserted parameters please. Is missing something"
+            sys.exit(1)
+
+        # This implementation is for compatibility with python 2.6 from the past machines!!
+        p = Popen([openssl_cmd], stdout=PIPE, shell=True, stderr=subprocess.STDOUT )
+        proc = p.communicate()[0].split('\n')
+    
+        if 'unable to load certificate' in proc[0]: # Preventing to see the ugly error from openssl command
+            print 'Unable to load certificate. Check entered host and try again'
+            sys.exit(1)
+        else:
+            expiredate = proc[1].split('=')[1]   # Expiredate from certificate read with openssl
+            obj_expiredate = datetime.datetime.strptime(expiredate, '%b %d %H:%M:%S %Y %Z') # object transformation
+            diff_date = obj_expiredate - date_today # difference of obj_expiredate - date_today
+            print_to_stdout(diff_date,silent,obj_expiredate) # Output
